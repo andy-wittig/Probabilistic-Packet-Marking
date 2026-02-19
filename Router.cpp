98ac61e5948c68ebcad17863327c28ddbe3bcc18
andy-wittig <157990692+andy-wittig@users.noreply.github.com>
2026-02-18 19:36:05 -0800

    Create Router.cpp

---
+++ Router.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <array>
+
+#include "Response.cpp"
+#include "Request.cpp"
+
+using namespace std;
+
+class IPAddress
+{
+    public:
+        array<int, 4> address;
+        IPAddress(array<int, 4> ip) : address(ip) {}
+
+        void PrintAddress()
+        {
+            cout << address[0] << "."
+                 << address[1] << "."
+                 << address[2] << "."
+                 << address[0];
+        }
+
+        bool operator==(const IPAddress& other) const
+        {
+            return address == other.address;
+        }
+};
+
+struct Packet
+{
+    int hopCount = 0;
+    bool marked = false;
+    IPAddress source;
+    IPAddress destination;
+    Packet(IPAddress s, IPAddress d) : source(s), destination(d) {}
+};
+
+class Router
+{
+    private:
+        IPAddress _ipAddress;
+        vector<Router*> neighborRouters;
+        float _markProbability;
+    public:
+        Router(array<int, 4> ip, float markProbability) : _ipAddress(ip), _markProbability(markProbability)
+        {
+            assert(markProbability >= 0 && markProbability <= 1 && "Probability 'markProbability' must be a float between 0 and 1.");
+        }
+
+        void Connect(Router* other)
+        {
+            neighborRouters.push_back(other);
+        }
+
+        bool GetMarkingProbability()
+        { //Returns true if packet is set to be marked.
+            float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+            return (r <= _markProbability);
+        }
+
+        void ForwardPacket(Packet& packet)
+        {
+            //Probability of marking packet.
+            if (!packet.marked && GetMarkingProbability())
+            {
+                packet.marked = true;
+                cout << "Router @: ";
+                _ipAddress.PrintAddress();
+                cout << " marked the packet." << endl;
+            }
+
+            packet.hopCount++;
+
+            if (_ipAddress == packet.destination)
+            {
+                cout << "Packet Reached Destination @: ";
+                _ipAddress.PrintAddress();
+                cout << " in " << packet.hopCount << "hops." << endl;
+            }
+
+            //Forwarding Logic
+            if (!neighborRouters.empty())
+            { //Forward to first neighbor.
+                neighborRouters[0]->ForwardPacket(packet);
+            }
+        }
+
+        IPAddress RequestAddress()
+        {
+            return _ipAddress;
+        }
+};
