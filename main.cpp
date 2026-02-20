d17589e8075493c20f3e7461648c7dfda4ae10f5
andy-wittig <Awittig@nevada.unr.edu>
2026-02-19 22:15:24 -0800

    Threading attempt.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <cstdlib>
-#include <ctime>
 #include <iostream>
+#include <thread>
+#include <ctime>
+#include <limits>
+#include <vector>
 
 #include "Router.h"
+
+using namespace std;
+
+void SendPacket(Router& source, Router& destination)
+{
+    Packet packet(source.RequestAddress(), destination.RequestAddress());
+    source.ForwardPacket(packet);
+}
 
 int main()
 {
@@ -12,14 +23,18 @@ int main()
     cout << "Please enter a marking probability (p): ";
     while (!(cin >> markingProbability))
     {
-        cout << "The input provided was not a valid number" << endl;
+        cout << "The input provided was not a valid number.";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 
-    float attackerRate;
+    int attackerRate;
     cout << "Please enter the attackers rate (x): ";
     while (!(cin >> attackerRate))
     {
-        cout << "The input provided was not a valid number" << endl;
+        cout << "The input provided was not a valid number.";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 
     //-------------------
@@ -30,14 +45,15 @@ int main()
     // Attacker rate of sending packets is x times more than normal users.
     //-------------------
     Router attacker({192, 168, 1, 1}, markingProbability);
-    Router r2({192, 168, 1, 2}, markingProbability);
-    Router r3({192, 168, 1, 3}, markingProbability);
-    Router r4({192, 168, 1, 4}, markingProbability);
-    Router victim({192, 168, 1, 5}, markingProbability);
+    Router r1({192, 169, 1, 2}, markingProbability);
+    Router r2({192, 168, 1, 3}, markingProbability);
+    Router r3({192, 168, 1, 4}, markingProbability);
+    Router r4({192, 168, 1, 5}, markingProbability);
+    Router victim({192, 168, 1, 6}, markingProbability);
 
     //---Network Topology---
-    //      attacker
-    //         /
+    //      R1  attacker
+    //        \/
     //        R2 
     //       /  \
     //      R3   R4
@@ -46,6 +62,7 @@ int main()
     //---------------------
 
     attacker.Connect(&r2);
+    r1.Connect(&r2);
     r2.Connect(&r3);
     r2.Connect(&r4);
     r3.Connect(&victim);
@@ -54,10 +71,30 @@ int main()
 
     //-------------------
     // Packet Forwarding
+    // - Routers should have single thread allocated for use
+    // - Routers use queues to process packets
     //-------------------
-    Packet packet(attacker.RequestAddress(), victim.RequestAddress());
-    attacker.ForwardPacket(packet);
 
-    system("pause");
+    int baseTasks = 1;
+    int attackerTasks = baseTasks * attackerRate;
+    vector<thread> forwardPacketThreads;
+    forwardPacketThreads.reserve(baseTasks + attackerTasks); //Eliminates memory reallocation for every task added.
+    
+    for (int i = 0; i < baseTasks; i++)
+    { //Normal Use Packets
+        forwardPacketThreads.emplace_back(SendPacket, ref(r1), ref(victim));
+    }
+    for (int i = 0; i < attackerTasks; i++)
+    { //Attacker Packets
+        forwardPacketThreads.emplace_back(SendPacket, ref(attacker), ref(victim));
+    }
+
+    for (auto& t : forwardPacketThreads)
+    {
+        t.join();
+    }
+
+    cin.get();
+    cin.get();
     return 0;
 };
